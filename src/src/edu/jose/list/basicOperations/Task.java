e802f7df2f7b458f7423d6c3c75f7dca5b3d0ae8
José Ruan <jrpaivadev@gmail.com>
2024-09-12 01:13:11 -0300

    feat: add task classes and task list

---
+++ src/src/edu/jose/list/basicOperations/Task.java
@@ -0,0 +1,19 @@
+package edu.jose.list.basicOperations;
+
+public class Task {
+
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
